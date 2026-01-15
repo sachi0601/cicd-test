6953c30dea641092d2caa7b4dcd439c7216b53ec
sachi <sachio.exe.uver@gmail.com>
2026-01-15 06:03:19 +0000

    test

--- app/src/main/java/sample/DemoApplication.java
+++ app/src/main/java/sample/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        System.out.println("Hello, World! from AWS Batch riv2 ");
+        System.out.println("Hello, World! from AWS Batch riv3 ");
         System.exit(0); // ← Batchジョブ即時終了
     }
 }
